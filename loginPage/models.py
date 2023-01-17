546463e6f16da71a987aee5d0dcbe84242b48343
Dhrumil Sheth <dhrumilsheth1512@gmail.com>
2023-01-17 13:45:45 +0530

    models for authentication

--- loginPage/models.py
+++ loginPage/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class students_login_credentials(models.Model):
+    username = models.CharField(max_length=50, unique=True, null=False, primary_key=True)
+    password = models.CharField(max_length=20, unique=True, null=False)
+
+class faculty_login_credentials(models.Model):
+    username = models.CharField(max_length=50, unique=True, null=False, primary_key=True)
+    password = models.CharField(max_length=20, unique=True, null=False)
+    
+class admin_or_hod_login_credentials(models.Model):
+    username = models.CharField(max_length=50, unique=True, null=False, primary_key=True)
+    password = models.CharField(max_length=20, unique=True, null=False)
