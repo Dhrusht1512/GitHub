0904ab00d190e8a4a35f43c2184fd6ea6f5f1043
Bhoomi Baraiya <baraiyabhoomi23@gmail.com>
2023-01-20 22:07:15 +0530

    Done some changes to models, admin and settings files.

--- loginPage/admin.py
+++ loginPage/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+class CustomAdmin(UserAdmin):
+    list_display = ['username', 'user_type']
+
+
+admin.site.register(CustomUser)
--- loginPage/models.py
+++ loginPage/models.py
@@ -1,14 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class CustomUser(models.Model):
+    USER_TYPE = [
+        (1, 'HOD'),
+        (2, 'Staff'),
+        (3, 'Student')
+    ]
+    user_type = models.CharField(max_length=255, choices=USER_TYPE)
+    profile_pic = models.FileField(upload_to='media/profile_pic')
+
+
 class students_login_credentials(models.Model):
-    username = models.CharField(max_length=50, unique=True, null=False, primary_key=True)
+    username = models.CharField(
+        max_length=255, unique=True, null=False, primary_key=True)
     password = models.CharField(max_length=20, unique=True, null=False)
 
+
 class faculty_login_credentials(models.Model):
-    username = models.CharField(max_length=50, unique=True, null=False, primary_key=True)
+    username = models.CharField(
+        max_length=255, unique=True, null=False, primary_key=True)
     password = models.CharField(max_length=20, unique=True, null=False)
-    
+
+
 class admin_or_hod_login_credentials(models.Model):
-    username = models.CharField(max_length=50, unique=True, null=False, primary_key=True)
+    username = models.CharField(
+        max_length=255, unique=True, null=False, primary_key=True)
     password = models.CharField(max_length=20, unique=True, null=False)
